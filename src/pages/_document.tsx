63021196f900c391d4c2f86f39aae01f28afb403
Jorge Felipe <jfss1@ifal.edu.br>
2022-06-20 09:20:52 -0300

    create page posts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,40 @@
-import Document from 'next/document';
+import { ReactElement } from 'react';
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;600;800;900&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +24,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  console.log(postsPagination.results);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  return (
+    <main className={styles.container}>
+      <div className={styles.posts}>
+        {postsPagination.results.map(post => (
+          <div className={styles.post}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.subtitle}</p>
+            <div>
+              <span>{post.data.author}</span>
+              <span>{post.first_publication_date}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getAllByType('post');
+
+  const posts = postsResponse.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author),
+      },
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        results: posts,
+      },
+    },
+  };
+};
